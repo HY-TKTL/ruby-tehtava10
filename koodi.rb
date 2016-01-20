16a0ffeb085cfbb88f90d63d21eead5ec3f4a414
Juho <juho.kyyhkynen@helsinki.fi>
2016-01-20 10:50:03 +0200

    teht10

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,11 @@
+def avainten_summa hash
+  summa = hash.keys.inject(:+)
+  if summa.nil?
+    0
+  else
+    summa
+  end
+end
 # kirjoita koodi tänne
+hsh = {1 => 1, 2 => 2, 3 => 3}
+puts "#{avainten_summa hsh}"
