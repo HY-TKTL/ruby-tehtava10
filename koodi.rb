543ae1effb7919601aeb48f94402c736ffbf0674
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-01-13 11:16:17 +0200

    opin käymään hashin avaimet läpi.

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,8 @@
-# kirjoita koodi tänne
+def avainten_summa(hash)
+   palautus = 0
+   hash.each_key {|a| palautus += a}
+   return palautus
+   end
+
+
+hash = { 1 => "jee" , 2 => "kaksi" , 3 => "kolme"}
